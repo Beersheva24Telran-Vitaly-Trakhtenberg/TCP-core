8e3f3bacbdf89c9744da02c2af2e1f76b1dec7ce
Vitaly Trakhtenberg <webdev@tca.by>
2024-11-17 16:13:57 +0300

    catch modified

--- Modified-Answer-TCP/tcp-server/src/main/java/telran/net/Main.java
+++ Modified-Answer-TCP/tcp-server/src/main/java/telran/net/Main.java
@@ -28,7 +28,7 @@ public class Main
                 server.run();
             } catch (Exception e) {
                 System.out.println("Client closed connection abnormally");
-                System.err.println(e.getStackTrace());
+                System.err.println(String.format("%s \n %s", e.getMessage(), e.getStackTrace().toString()));
             }
         }
     }
