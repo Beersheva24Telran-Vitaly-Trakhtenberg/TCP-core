d1e6ea3d083c262ad5676ed8ff743291894bbd4b
Vitaly Trakhtenberg <webdev@tca.by>
2024-11-17 16:04:28 +0300

    removed redundant messages and pon-dependencies

--- Modified-Answer-TCP/tcp-server/src/main/java/telran/net/Main.java
+++ Modified-Answer-TCP/tcp-server/src/main/java/telran/net/Main.java
@@ -29,50 +29,11 @@ public class Main
         while(true) {
             try {
                 TCPClientServiceSession session = server.accept();
-                runSession(session);
+                //runSession(session);
             } catch (IOException e) {
                 System.out.println("Client closed connection abnormally");
             }
         }
     }
 
-    private static void runSession(TCPClientServiceSession session)
-    {
-        try (BufferedReader in = new BufferedReader(session.getInputStream());
-        PrintStream out = new PrintStream(session.getOutputStream())) {
-            String line;
-            while((line = in.readLine()) != null) {
-                String[] tmp_arr = line.split("#");
-                if (tmp_arr.length > 1) {
-                    if (tmp_arr[0].equals("R")) {
-                        line = input_reverse(tmp_arr[1]);
-                    } else if (tmp_arr[0].equals("L")) {
-                        line = input_length(tmp_arr[1]);
-                    } else {
-                        line = "Modificator '" + tmp_arr[0] + "' Undefined!";
-                    }
-                }
-                out.printf("Echo Server sends back: '%s'\n", line);
-                System.out.println(line);
-            }
-        } catch (Exception e) {
-            System.out.println("Client closed connection abnormally");
-        }
-    }
-
-    private static String input_length(String s)
-    {
-        return String.format("Length of input '%s' is %d",s,s.length());
-    }
-
-    private static String input_reverse(String s)
-    {
-        int max = s.length();
-        String res = "";
-        for (int i=max-1; i>=0; i--) {
-            String symb = s.substring(i, i+1);
-            res = res.concat(symb);
-        }
-        return res;
-    }
 }
