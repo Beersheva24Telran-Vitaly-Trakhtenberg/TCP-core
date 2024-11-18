39eb37a89abc86cc634b6dddf2a655db4cf4a485
Vitaly Trakhtenberg <webdev@tca.by>
2024-11-18 20:49:39 +0300

    Time to get things in order for TCP-NET library git

--- tcp-net/src/main/java/telran/net/TCPClientServiceSession.java
+++ tcp-net/src/main/java/telran/net/TCPClientServiceSession.java
@@ -1,4 +1,51 @@
 package telran.net;
 
-public class TCPClientServiceSession {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.*;
+
+public class TCPClientServiceSession implements Runnable
+{
+    Protocol protocol;
+    Socket socket;
+    PrintStream out;
+    BufferedReader in;
+
+    public TCPClientServiceSession(Protocol protocol, Socket socket)
+    {
+        this.protocol = protocol;
+        this.socket = socket;
+    }
+    /**
+     * Runs this operation.
+     */
+    @Override
+    public void run()
+    {
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintStream(socket.getOutputStream());
+            String request = null;
+            while ((request = in.readLine()) != null) {
+                String response = protocol.getResponseWithJSON(request);
+                out.println(response);
+            }
+            socket.close();
+        } catch (Exception e) {
+            System.out.println("Error processing request: " + e.getMessage());
+            System.err.println(e);
+        }
+    }
+
+    public BufferedReader getInputStream() throws IOException
+    {
+        return in;
+    }
+
+    public PrintStream getOutputStream() throws IOException
+    {
+        return out;
+    }
 }
--- tcp-net/src/main/java/telran/net/TCPServer.java
+++ tcp-net/src/main/java/telran/net/TCPServer.java
@@ -1,4 +1,42 @@
 package telran.net;
 
-public class TCPServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer implements Runnable
+{
+    Protocol protocol;
+    int port;
+    ServerSocket server_socket;
+
+    public TCPServer(Protocol protocol, int port)
+    {
+        this.protocol = protocol;
+        this.port = port;
+    }
+
+    /**
+     * Runs this operation.
+     */
+    @Override
+    public void run()
+    {
+        try {
+            server_socket = new ServerSocket(port);
+            System.out.println("Server started on port " + port);
+            while (true) {
+                this.accept().run();
+            }
+        } catch (Exception e) {
+            System.out.println("Error on the server: " + e.getMessage());
+            System.err.println(e);
+        }
+    }
+
+    public TCPClientServiceSession accept() throws IOException
+    {
+        Socket socket = server_socket.accept();
+        return new TCPClientServiceSession(protocol, socket);
+    }
 }
