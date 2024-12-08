21585c31c3e20f94e2afc938aee88619a1096148
Vitaly Trakhtenberg <webdev@tca.by>
2024-12-08 21:29:59 +0200

    added SocketTimeOut handling for shutdown  the ExecutorService framework to provide shutdownNow (to ignore all not processing client sessions)

--- tcp-net/src/main/java/telran/net/TCPServer.java
+++ tcp-net/src/main/java/telran/net/TCPServer.java
@@ -3,17 +3,25 @@ package telran.net;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class TCPServer implements Runnable
 {
     private Protocol protocol;
     private int port;
-//    ServerSocket server_socket;
+    private final ExecutorService executorService;
+    private static final int MAX_CONNECTIONS = 10;
+    private static final int IDLE_CONNECTON_MS_TIMEOUT = 30000;
 
     public TCPServer(Protocol protocol, int port)
     {
         this.protocol = protocol;
         this.port = port;
+        this.executorService = Executors.newFixedThreadPool(MAX_CONNECTIONS);
+
     }
 
     /**
@@ -21,24 +29,42 @@ public class TCPServer implements Runnable
      */
     @Override
     public void run() {
-        //TODO add SocketTimeOut handling for shutdown
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is listening on the port "+ port);
-            while(true) {
-                Socket socket = serverSocket.accept();
-                var session = new TCPClientServerSession(protocol, socket);
-                Thread thread = new Thread(session);
-
-                thread.start();
+            while(!executorService.isShutdown()) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    socket.setSoTimeout(IDLE_CONNECTON_MS_TIMEOUT);
+                    var session = new TCPClientServerSession(protocol, socket);
+                    executorService.submit(session);
+/*
+                } catch (SocketTimeoutException e) {
+                    //ignore
+*/
+                } catch (IOException e) {
+                    if (executorService.isShutdown()) {
+                        break;
+                    }
+                    System.out.println(e);
+                }
             }
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public void shutdown() {
-        //TODO
-        //In the ExecutorService framework to provide shutdownNow (to ignore all not processing client sessions)
+    public void shutdown()
+    {
+        System.out.println("Shutdown initiated...");
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+        System.out.println("Shutdown complete.");
     }
 
 }
