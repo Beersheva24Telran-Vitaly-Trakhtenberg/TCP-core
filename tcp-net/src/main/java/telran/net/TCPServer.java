c458fc3b496a13dd6467f7b1844952a256dca33e
Vitaly Trakhtenberg <webdev@tca.by>
2024-12-10 16:52:30 +0200

    Added additional constructors (took idea from the Yuri's repo & class session)

--- tcp-net/src/main/java/telran/net/TCPServer.java
+++ tcp-net/src/main/java/telran/net/TCPServer.java
@@ -3,6 +3,7 @@ package telran.net;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -12,15 +13,44 @@ public class TCPServer implements Runnable
     private final Protocol protocol;
     private final int port;
     private final ExecutorService executorService;
-    private static final int MAX_CONNECTIONS = 10;
-    private static final int IDLE_CONNECTON_MS_TIMEOUT = 30000;
 
-    public TCPServer(Protocol protocol, int port)
+    public TCPServer(Protocol protocol,
+                     int port,
+                     int socketTimeout,
+                     int idleConnectionTimeout,
+                     int limitRequestsPerSecond,
+                     int limitNonOkResponsesInRow)
     {
         this.protocol = protocol;
         this.port = port;
-        this.executorService = Executors.newFixedThreadPool(MAX_CONNECTIONS);
 
+        TCPServerSettings.setSocketTimeout(socketTimeout);
+        TCPServerSettings.setIdleConnectionTimeout(idleConnectionTimeout);
+        TCPServerSettings.setLimitNonOkResponsesInRow(limitNonOkResponsesInRow);
+        TCPServerSettings.setLimitRequestsPerSecond(limitRequestsPerSecond);
+        TCPServerSettings.setLimitNonOkResponsesInRow(limitNonOkResponsesInRow);
+
+        this.executorService = Executors.newFixedThreadPool(TCPServerSettings.getMaxConnectionsNumber());
+    }
+
+    public TCPServer(Protocol protocol, int port, int idleConnectionTimeout)
+    {
+        this(protocol,
+            port,
+            TCPServerSettings.getSocketTimeout(),
+            idleConnectionTimeout,
+            TCPServerSettings.getLimitRequestsPerSecond(),
+            TCPServerSettings.getLimitNonOkResponsesInRow());
+    }
+
+    public TCPServer(Protocol protocol, int port)
+    {
+        this(protocol,
+            port,
+                TCPServerSettings.getSocketTimeout(),
+                TCPServerSettings.getIdleConnectionTimeout(),
+                TCPServerSettings.getLimitRequestsPerSecond(),
+                TCPServerSettings.getLimitNonOkResponsesInRow());
     }
 
     /**
@@ -33,7 +63,7 @@ public class TCPServer implements Runnable
             while(!executorService.isShutdown()) {
                 try {
                     Socket socket = serverSocket.accept();
-                    socket.setSoTimeout(IDLE_CONNECTON_MS_TIMEOUT);
+                    socket.setSoTimeout(TCPServerSettings.getIdleConnectionMsTimeout());
                     var session = new TCPClientServerSession(protocol, socket);
                     executorService.submit(session);
                 } catch (IOException e) {
@@ -48,18 +78,23 @@ public class TCPServer implements Runnable
         }
     }
 
-    public void shutdown()
+    public boolean shutdown()
     {
         System.out.println("Shutdown initiated...");
+        boolean res = true;
         executorService.shutdown();
         try {
             if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
                 executorService.shutdownNow();
+                res = false;
             }
         } catch (InterruptedException e) {
             executorService.shutdownNow();
+            res = false;
         }
         System.out.println("Shutdown complete.");
+
+        return res;
     }
 
 }
